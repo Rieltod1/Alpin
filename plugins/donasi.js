7cfb5cd74f96a80f1adb1a7f4fe0648b33df2e69
Rieltod1 <106787620+Rieltod1@users.noreply.github.com>
2022-06-03 08:00:19 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,13 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [082287750102]
-│ • Dana  [082287750102]
+│ • axis [6283813417529]
+│ • Dana  [6283813417529]
 │ • Ovo [082287750102]
 │ • Gopay [082287750102]
-│ • https://saweria.co/itsmedell
+│ • https://saweria.co/
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi/sewa? Wa.me/6282287750102
+│ > Ingin donasi/sewa? Wa.me/6282146218274
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24 jam tanpa kendala_
